a651377b8c5025a23d0125588fbc25fdc9b753fd
KNNent <164649785+KNNent@users.noreply.github.com>
2024-03-24 00:31:27 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+    // Set a timeout to hide the preloader after 3 seconds
+    var mainContent = document.getElementById('content');
+    setTimeout(function() {
+      document.getElementById('preloader').style.display = 'none';
+      document.getElementById('content').style.visibility = 'visible';
+      mainContent.classList.add('fade-in');
+    }, 3000); // 3000 milliseconds = 3 seconds
+  });
